2ce0493e8c67d997b7d6875b345a0f6a2ac88a80
goindol <goindol@live.com>
2017-04-21 11:14:55 +0900

    hello.java 두번째 커밋을 위한 2행 추가

--- HelloWorld/src/hello/HelloWorld.java
+++ HelloWorld/src/hello/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		// 두번째 커밋을 위한 코드 추가
+		System.out.println("2nd commit message.");
 	}
 
 }
